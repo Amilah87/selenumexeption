2f8183f0aa10877494ac6db4240662b9a6c33d88
Amilah87 <udamila87@gmail.com>
2018-11-18 12:16:18 +0530

    update

---
+++ src/main/java/selenumexeption/SearchGoogle.java
@@ -0,0 +1,57 @@
+package selenumexeption;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.NoSuchWindowException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class SearchGoogle {
+	
+	//WebDriver driver;
+
+	@Test
+
+		public void testSearch()
+		{
+		try {
+		    WebDriver driver;
+		    System.setProperty("webdriver.chrome.driver", "C:\\chromedriver_win32\\chromedriver.exe");
+			driver = new ChromeDriver();
+			driver.get("http://www.google.lk");
+			
+			
+			driver.findElement(By.id("lst-ib1")).sendKeys("Selenium");
+			driver.findElement(By.name("btnK")).click();
+			driver.findElement(By.linkText("Selenium - Web Browser Automation")).click();
+			
+			String webTitle = driver.getTitle();
+			
+			Assert.assertEquals(webTitle, "Selenium - Web Browser Automation");
+			driver.close();
+			driver.quit();
+		  }
+		
+	  
+	    	catch (NoSuchElementException e) 
+		    {
+		    	System.out.println("Element was not found sorry");
+		    }
+			catch (NoAlertPresentException e) 
+		    {
+		    	System.out.println("Alert was not found sorry");
+		    }
+			catch (NoSuchWindowException e) 
+		    {
+		    	System.out.println("Window was not found sorry");
+		    }
+			
+		}
+
+	
+	
+
+}
